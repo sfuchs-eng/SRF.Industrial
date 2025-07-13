412449c823169342e3469988a9b9252dc94a22d8
Stefan Fuchs <sfuchs@sfuchs.ch>
2025-07-13 20:06:37 +0200

    new prj

---
+++ Modbus/ModbusClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SRF.Industrial.Modbus;
+
+public class ModbusClient : IDisposable
+{
+    private readonly TcpClient tcpClient;
+
+    public ModbusClient(ModbusClientConfig config)
+    {
+        this.tcpClient = new TcpClient();
+    }
+
+    public async void Connect(string host, ushort port)
+    {
+        if (string.IsNullOrEmpty(host))
+            throw new ArgumentNullException(nameof(host));
+
+        await tcpClient.ConnectAsync()
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}

550e439a78320287e0f002c1eab69594a7cc33bd
Stefan Fuchs <sfuchs@sfuchs.ch>
2025-08-02 20:42:34 +0200

    Modbus.Test initialized

---
+++ Modbus.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace SRF.Industrial.Modbus.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
